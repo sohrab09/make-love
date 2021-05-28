de96dcda163f76c7161d0610876bb6c1cf9898a9
Fazle Rabbi <24.fazlerabbi@gmail.com>
2021-05-28 17:31:54 +0600

    adding home

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         </p>
         <h1>This header branch</h1>
         <h2>This command from main branch</h2>
+        <h2>This is home</h2>
       </header>
     </div>
   );
