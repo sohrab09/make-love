49c0cb6ae7042ce9e1eb20c1d820ccdc3a73d372
Fazle Rabbi <24.fazlerabbi@gmail.com>
2021-05-28 18:13:26 +0600

    adding sitebar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <h1>This header branch</h1>
         <h2>This command from main branch</h2>
         <h2>This is home</h2>
+        <h1>This is siteBar</h1>
       </header>
     </div>
   );
